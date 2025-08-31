f4507e07f5e6910bfb072b3d175424d1888a3d82
Vaibhav <thevaibhavdixit@gmail.com>
2025-08-31 17:47:31 +0530

    understanding write() with fork()

---
+++ interlude_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+  int fd = open("./temp_file_to_write.txt", O_CREAT|O_WRONLY|, S_IRWXU);
+
+  if(fd < 0) {
+    fprintf(stderr, "open failed\n");
+    exit(1);
+  }
+
+  int rc = fork();
+  if (rc < 0) {
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+  }
+  else if (rc == 0) {
+    write(fd, "hello\n", 6);
+    close(fd);
+  }
+  else {
+    write(fd, "goodbye\n", 8);
+    close(fd);
+  }
+}
