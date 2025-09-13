ae41672934fbed49436aa2c2c961720e67e6b09a
Vaibhav <thevaibhavdixit@gmail.com>
2025-09-13 22:36:47 +0530

    using symbolic constants

--- temp_conv_for_loops.c
+++ temp_conv_for_loops.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+// symbolic constants
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+
 int main() {
-    for (int fahr = 0; fahr <= 300; fahr += 20) {
+    for (int fahr = LOWER; fahr <= UPPER; fahr += STEP) {
         float celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%3d %6.1f\n", fahr, celsius);
     }
