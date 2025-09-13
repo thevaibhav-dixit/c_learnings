1907c831253c1676a8dd41b08286540510022a73
Vaibhav <thevaibhavdixit@gmail.com>
2025-09-13 22:21:38 +0530

    example with floating numbers

---
+++ temp_conv_floating_version.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      // lower limit of temperature scale
+    upper = 300;    // upper limit
+    step = 20;      // step size
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius); // %6.1f for floating point with 1 decimal place
+        fahr = fahr + step;
+    }
+}
