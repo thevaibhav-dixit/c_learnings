09414f0b6cfb2857b6cd7320746fd08f28f83449
Vaibhav <thevaibhavdixit@gmail.com>
2025-09-16 23:25:20 +0530

    input related exercises

--- input.c
+++ input.c
@@ -1,10 +1,18 @@
 #include <stdio.h> 
 
+void valueOfEOF() {
+  //-1
+  printf("Value of EOF: %d\n", EOF);
+}
+
 int main() {
-  int c; 
-  c = getchar(); 
-  while (c != EOF) { 
-    putchar(c);
-    c = getchar(); 
-  }
+  /* int c; */
+  /* c = getchar(); */
+  /* while (c != EOF) { */
+  /*   putchar(c); */
+  /*   c = getchar(); */
+  /* } */
+
+  int res = getchar() != EOF;
+  printf("getchar() != EOF: %d\n", res);
 }
